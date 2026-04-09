d1a717e77226dba783ada2c9935f219bf2e6027a
Dorian Weber <dorianweb57@gmail.com>
2026-04-09 14:58:55 +0200

    feat(Pages) : ajout des pages d'accueil et des Evenements en html, CSS, tailwinds

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Events } from './pages/events/events';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path :'', component : Home},
+  {path :'events', component : Events}
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,20 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIf, RouterLink],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('Gest-Club-Sport-Frontend');
+
+  // 👉 Signal pour ouvrir / fermer le menu mobile
+  menuOpen = signal(false);
+
+  toggleMenu() {
+    this.menuOpen.update((v) => !v);
+  }
 }
