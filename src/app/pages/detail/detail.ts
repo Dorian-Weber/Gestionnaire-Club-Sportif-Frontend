4e35f214b5b37c67de151d12c369e86b7c91c116
Dorian Weber <dorianweb57@gmail.com>
2026-04-10 11:18:17 +0200

    feat(detail): ajout de la page détailé pour événement

---
+++ src/app/pages/detail/detail.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-detail',
+  imports: [RouterLink],
+  templateUrl: './detail.html',
+  styleUrl: './detail.css',
+})
+export class Detail {}
