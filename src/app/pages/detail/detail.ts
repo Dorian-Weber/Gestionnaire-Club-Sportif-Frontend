cd9373d30c5a5ef741f5530dc7e0539ef3683918
Dorian Weber <dorianweb57@gmail.com>
2026-04-11 17:37:43 +0200

    feat(button): ajout du composant button dans la page détail

--- src/app/pages/detail/detail.ts
+++ src/app/pages/detail/detail.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { Button } from '../../composants/button/button';
 
 @Component({
   selector: 'app-detail',
-  imports: [RouterLink],
+  imports: [RouterLink, Button],
   templateUrl: './detail.html',
   styleUrl: './detail.css',
 })
