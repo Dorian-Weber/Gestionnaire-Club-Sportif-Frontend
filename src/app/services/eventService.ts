26d44b45b19f64c7de42199bfb3e7d95c7493286
Dorian Weber <dorianweb57@gmail.com>
2026-04-14 13:58:24 +0200

    feat(eventService): ajout du service pour le model event

---
+++ src/app/services/eventService.ts
@@ -0,0 +1,16 @@
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Event } from '../models/Event';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class EventService {
+  http = inject(HttpClient);
+  apiUrl = 'http://localhost:8080/event';
+
+  getEvents(): Observable<Event[]> {
+    return this.http.get<Event[]>(`${this.apiUrl}/list`);
+  }
+}
