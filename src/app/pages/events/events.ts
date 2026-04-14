f7332a952610e5ffd4f0c7e203271456614f8f4a
Dorian Weber <dorianweb57@gmail.com>
2026-04-14 14:20:24 +0200

    feat(events) : ajout de la connexion a l'entité Sport pour préparer le tri

--- src/app/pages/events/events.ts
+++ src/app/pages/events/events.ts
@@ -4,6 +4,7 @@ import { Card } from '../../composants/card/card';
 import { EventService } from '../../services/eventService';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { EventTypeService } from '../../services/event-type-service';
+import { SportService } from '../../services/sport-service';
 
 @Component({
   selector: 'app-events',
@@ -12,9 +13,13 @@ import { EventTypeService } from '../../services/event-type-service';
   styleUrl: './events.css',
 })
 export class Events {
+  // Injection des services
   private eventService = inject(EventService);
   private eventTypeService = inject(EventTypeService);
+  private sportService = inject(SportService);
 
+  // Création des signaux
   events = toSignal(this.eventService.getEvents(), { initialValue: [] });
   eventTypes = toSignal(this.eventTypeService.getEventTypes(), { initialValue: [] });
+  sports = toSignal(this.sportService.getSports(), { initialValue: [] });
 }
---
+++ src/app/services/sport-service.ts
@@ -0,0 +1,16 @@
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Sport } from '../models/Sport';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SportService {
+  http = inject(HttpClient);
+  apiUrl = 'http://localhost:8080/sport';
+
+  getSports(): Observable<Sport[]> {
+    return this.http.get<Sport[]>(`${this.apiUrl}/list`);
+  }
+}
