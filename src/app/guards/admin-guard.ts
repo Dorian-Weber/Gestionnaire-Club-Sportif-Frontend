d701da2430fe2583795eb0f2bf711fbdd692362d
Dorian Weber <dorianweb57@gmail.com>
2026-05-26 16:42:59 +0200

    feat(guard) : ajour de la ligne qui permet de sauvegarder l'url dans tout les guards

--- src/app/guards/admin-guard.ts
+++ src/app/guards/admin-guard.ts
@@ -7,6 +7,9 @@ export const adminGuard: CanActivateFn = (route, state) => {
   const authService = inject(Auth)
 
   if(authService.jwtInfo()?.role != 'ADMIN' || authService.jwtInfo()?.role != 'SUPER_ADMIN') {
+
+    authService.redirectUrl = state.url;
+
     const router = inject(Router)
     return router.parseUrl('/login');
   }
--- src/app/guards/super-admin-guard.ts
+++ src/app/guards/super-admin-guard.ts
@@ -6,6 +6,9 @@ export const superAdminGuard: CanActivateFn = (route, state) => {
   const authService = inject(Auth);
 
   if (authService.jwtInfo()?.role != 'SUPER_ADMIN') {
+
+    authService.redirectUrl = state.url;
+
     const router = inject(Router);
     return router.parseUrl('/login');
   }
